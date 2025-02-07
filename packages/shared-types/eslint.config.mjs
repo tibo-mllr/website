f37a8a0f3efa23bdd15dc34c33c2594cf58d2176
Thibault Muller <thibault.muller19@gmail.com>
2025-02-07 19:34:47 +0100

    eslint: shared-types

---
+++ packages/shared-types/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import rootConfig from '../../eslint.config.mjs';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...rootConfig,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
