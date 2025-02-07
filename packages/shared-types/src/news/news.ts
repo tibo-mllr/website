70c77ef29166beae1bae4792508013d0c1a5ee78
Thibault Muller <thibault.muller19@gmail.com>
2025-02-07 20:19:31 +0100

    CI pipeline for PRs

--- packages/shared-types/src/news/news.ts
+++ packages/shared-types/src/news/news.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+
 import { frontUserSchema } from '../user';
 
 export const newsSchema = z.object({
--- packages/shared-types/src/project/project.ts
+++ packages/shared-types/src/project/project.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+
 import { organizationSchema } from '../organization';
 
 export enum ProjectType {
